739b20f082fe638bc56c4a49369f8b826a218b2a
Ian Eure <ian.eure@gmail.com>
2010-05-18 14:48:28 -0700

    Imported Upstream version 0.4

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+
+c1=Extension('_geohash',
+	sources=['src/geohash.c',],
+	define_macros = [('PYTHON_MODULE',1),])
+
 setup(name='python-geohash',
-	version='0.2',
-	py_modules=['geohash','quadtree','jpgrid','jpiarea']
+	version='0.4',
+	description='Fast, accurate python geohashing library',
+	author='Hiroaki Kawai',
+	url='http://code.google.com/p/python-geohash/',
+	py_modules=['geohash','quadtree','jpgrid','jpiarea'],
+	ext_modules = [c1]
 )
