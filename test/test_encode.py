a671df23f15aa5cde72289c21e5228b94820544a
Y. T. Chung <zonyitoo@gmail.com>
2014-05-18 01:11:57 +0800

    ported to python 3.4.0

--- test/test_encode.py
+++ test/test_encode.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals, \
+    division, print_function
+
 import unittest
 import geohash
 
-class TestEncode(unittest.TestCase):
-	def test_cycle(self):
-		for code in ["000000000000","zzzzzzzzzzzz","bgr96qxvpd46",]:
-			self.assertEqual(code, geohash.encode(*geohash.decode(code)))
 
-# if __name__=='__main__':
-# 	unittest.main()
+class TestEncode(unittest.TestCase):
+
+    def test_cycle(self):
+        for code in ["000000000000", "zzzzzzzzzzzz", "bgr96qxvpd46", ]:
+            self.assertEqual(code, geohash.encode(*geohash.decode(code)))
+
+if __name__ == '__main__':
+    unittest.main()
